73f8e7f54baf7b67ebe6c057fe204165d928305a
Minhaz <sharif35-3001@diu.edu.bd>
2023-12-11 17:31:25 +0600

    feat: predict route added

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from flask import Flask, jsonify
-import camelcase
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    cr7 = camelcase.CamelCase()  # cr7 === camel ∞
-    idolsCommitment = cr7.hump("data segmentation map")
-    
+def test():
+    text = "Model is running perfectly"
     responseData = {
+        "status": 200,
         "success": True,
-        "message": idolsCommitment,
+        "message": text,
     }
     
     return jsonify(responseData)
 
+@app.route("/predict")
+def predict():
+    responseData = {
+        "status": 200,
+        "success": True,
+        "message": "Prediction result: 'Here'",
+    }
+    
+    return jsonify(responseData)
+
+
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
+    # app.run(debug=True, host="0.0.0.0", port=5001)
